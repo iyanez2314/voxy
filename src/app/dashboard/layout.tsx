ff058a04352e6b6eaff839d49e0edea3096142cb
Isaac Yanez <isaac231467@gmail.com>
2024-06-15 21:52:47 -0500

    fixed a few more pages to adhear to the new layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import SideNav from "../ui/side-nav";
+import Header from "../ui/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <SideNav />
-      <div className="flex flex-col w-full h-full">{children}</div>
+      <div className="flex flex-col w-full h-full">
+        <Header />
+        <main className="grid bg-[#242627] rounded-3xl flex-1 items-start gap-4 p-8 sm:px-6 sm:p-12 md:gap-8 lg:grid-cols-2 xl:grid-cols-1">
+          <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+            {children}
+          </div>
+        </main>
+      </div>
     </>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,47 @@
-import React from "react";
-import { MyDashboard } from "../components/MyDashboard";
+"use client";
+import React, { useState } from "react";
+import Dashboard from "../components/Dashboard/Dashboard";
+import { SessionType } from "@/src/types";
+let mockData: SessionType[] = [
+  {
+    id: "1",
+    grade: "90",
+    description: "Session 1",
+    createdAt: "2021-09-10T10:00:00.000Z",
+    updatedAt: "2021-09-10T10:00:00.000Z",
+  },
+  {
+    id: "2",
+    grade: "80",
+    description: "Session 2",
+    createdAt: "2021-09-10T10:00:00.000Z",
+    updatedAt: "2021-09-10T10:00:00.000Z",
+  },
+  {
+    id: "3",
+    grade: "70",
+    description: "Session 3",
+    createdAt: "2021-09-10T10:00:00.000Z",
+    updatedAt: "2021-09-10T10:00:00.000Z",
+  },
+];
 
 export default function Page() {
+  const [activeComponent, setActiveComponent] = useState("dashboard");
+  const [activeSession, setActiveSession] = useState<SessionType[] | undefined>(
+    []
+  );
+  const [activeSessionId, setActiveSessionId] = useState<string>("");
+
+  const handleSetActiveSession = (id: string) => {
+    const session = mockData.find(
+      (session) => session.id === id
+    ) as SessionType;
+    setActiveSession([session]);
+    setActiveSessionId(id);
+    setActiveComponent("session");
+  };
   return (
-    <>
-      <MyDashboard />
-    </>
+    <Dashboard data={mockData} handleActiveSession={handleSetActiveSession} />
   );
 }
