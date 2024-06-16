8747bf9e4b8383a6f29c750d9c9929bcfba49a10
Isaac Yanez <isaac231467@gmail.com>
2024-06-16 13:10:29 -0500

    fixed the layout for the web app to match the new structure

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,9 +4,9 @@ import Header from "../ui/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <SideNav />
-      <div className="flex flex-col w-full h-full">
+      <div className="sm:ml-14">
         <Header />
         <main className="grid bg-[#242627] rounded-3xl flex-1 items-start gap-4 p-8 sm:px-6 sm:p-12 md:gap-8 lg:grid-cols-2 xl:grid-cols-1">
           <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
@@ -14,6 +14,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           </div>
         </main>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/ui/side-nav.tsx
+++ src/app/ui/side-nav.tsx
@@ -12,7 +12,7 @@ import NavLinks from "./nav-links";
 
 export default function SideNav() {
   return (
-    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col  sm:flex">
+    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
         <div className="group flex shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
           <Image src={VoxyLogo} alt="Voxy Logo" />
