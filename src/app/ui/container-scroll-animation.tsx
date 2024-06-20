6adb10a6811dfae64e13cfa8b43fc9d4e6685158
Isaac Yanez <isaac231467@gmail.com>
2024-06-19 20:58:20 -0500

    fixed a ui bug

--- src/app/ui/container-scroll-animation.tsx
+++ src/app/ui/container-scroll-animation.tsx
@@ -36,7 +36,7 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[40rem] md:h-[60rem] flex items-center justify-center relative p-2 md:p-20"
+      className="h-[50rem] md:h-[60rem] flex items-center justify-center relative p-2 md:p-20"
       ref={containerRef}
     >
       <div
