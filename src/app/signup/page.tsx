7b8cb9719a658118004824ba81603e102803f075
Isaac Yanez <isaac231467@gmail.com>
2024-06-19 20:46:19 -0500

    fixed the navbar a bit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { SignUpHero } from "../components/Signup/SIgnUpHero";
 import VoxyLogo from "@/public/V-removebg-preview.png";
 import Image from "next/image";
-
 export default function Page() {
   return (
     <>
@@ -15,31 +14,7 @@ export default function Page() {
             <Image src={VoxyLogo} alt="Voxy Logo" width={100} height={100} />
           </Link>
 
-          <div className="flex flex-1 items-center justify-end md:justify-between">
-            <nav aria-label="Global" className="hidden md:block">
-              <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    About{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Price{" "}
-                  </a>
-                </li>
-              </ul>
-            </nav>
-
+          <div className="flex flex-1 justify-end">
             <div className="flex items-center gap-4">
               <div className="sm:flex sm:gap-4">
                 <Link
@@ -48,7 +23,6 @@ export default function Page() {
                 >
                   Login
                 </Link>
-
                 <a
                   className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-[#DB4A2B] transition hover:opacity sm:block"
                   href="#"
@@ -56,24 +30,6 @@ export default function Page() {
                   Register
                 </a>
               </div>
-
-              <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-                <span className="sr-only">Toggle menu</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
             </div>
           </div>
         </div>
