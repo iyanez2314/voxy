e205021349926bb48ee3815f1acf6ddd203c33cc
Isaac Yanez <isaac231467@gmail.com>
2024-06-27 19:50:34 -0500

    added the Speed insights and Analytics to the web app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -32,7 +34,11 @@ export default function RootLayout({
           />
         </head>
 
-        <body className="bg-[#151718]">{children}</body>
+        <body className="bg-[#151718]">
+          {children}
+          <SpeedInsights />
+          <Analytics />
+        </body>
       </html>
     </ClerkProvider>
   );
